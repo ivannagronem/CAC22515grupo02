07e7ef398b890955a891f20d6fedf0db6de56f43
ivannagronem <ivanna.grone@gmail.com>
2022-10-18 23:11:27 -0300

    Se agrega try catch de error en JS para API Rest

--- js/getexchangerate.js
+++ js/getexchangerate.js
@@ -1,20 +1,26 @@
 const url_api = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
     
 async function obtainData() {
-    const response = await fetch(url_api)
-    const data_er_total = await response.json()
-    const data_er_oficial = data_er_total[0].casa
+    try{    
+        const response = await fetch(url_api)
+        const data_er_total = await response.json()
+        const data_er_oficial = data_er_total[0].casa
 
-    const {nombre, compra, venta} = data_er_oficial
+        const {nombre, compra, venta} = data_er_oficial
 
-    console.log(data_er_oficial)
-    console.log(nombre)
-    console.log(compra)
-    console.log(venta)
+        console.log(data_er_oficial)
+        console.log(nombre)
+        console.log(compra)
+        console.log(venta)
 
-    document.getElementById("er_name").textContent=nombre
-    document.getElementById("er_buy").textContent=compra
-    document.getElementById("er_sell").textContent=venta
-
+        document.getElementById("er_name").textContent=nombre
+        document.getElementById("er_buy").textContent=compra
+        document.getElementById("er_sell").textContent=venta
+    } catch(e){
+        document.getElementById("er_name").textContent="Sin datos"
+        document.getElementById("er_buy").textContent="Sin datos"
+        document.getElementById("er_sell").textContent="Sin datos"
+        console.log(e.message)
+    }
 }
 obtainData()
